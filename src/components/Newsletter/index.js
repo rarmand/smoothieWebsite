2104a87add8d8196020e35896942422a9181c818
rarmand <holik.alek@gmail.com>
2019-06-10 15:44:08 +0200

    added hook in newsletter

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -2,49 +2,45 @@ import React from "react";
 import Net from "../../components/icons/Net";
 import "./styles.sass";
 
-class Newsletter extends React.Component {
-  state = {
-    email: ""
+function Newsletter() {
+  const [email, setEmail] = React.useState("");
+
+  const onInputChange = event => {
+    setEmail(event.target.value);
   };
 
-  onInputChange = event => {
-    this.setState({ email: event.target.value });
+  const onButtonClick = () => {
+    alert("Your email " + email + " is saved.");
+    setEmail("");
   };
 
-  onButtonClick = () => {
-    alert("Your email " + this.state.email + " is saved.");
-    this.setState({ email: "" });
-  };
-
-  render() {
-    return (
-      <div className="newsletterContent">
-        <Net color="#FF7956" className="newsletterContent__netIcon" />
-        <div className="newsletterContent__newsletterText">
-          <span className="newsletterText__span">
-            Stay tuned with our promotions, healthy advice and new products!
-          </span>
-          <input
-            className="newsletterText__input--text"
-            placeholder="Email"
-            type="text"
-            value={this.state.value}
-            onChange={this.onInputChange}
-          />
-          <button
-            className="newsletterText__button--submit"
-            type="button"
-            onClick={this.onButtonClick}
-          >
-            Subscribe to newsletter
-          </button>
-          <span className="newsletterText__span">
-            You can resign in any time.
-          </span>
-        </div>
+  return (
+    <div className="newsletterContent">
+      <Net color="#FF7956" className="newsletterContent__netIcon" />
+      <div className="newsletterContent__newsletterText">
+        <span className="newsletterText__span">
+          Stay tuned with our promotions, healthy advice and new products!
+        </span>
+        <input
+          className="newsletterText__input--text"
+          placeholder="Email"
+          type="text"
+          value={email}
+          onChange={onInputChange}
+        />
+        <button
+          className="newsletterText__button--submit"
+          type="button"
+          onClick={onButtonClick}
+        >
+          Subscribe to newsletter
+        </button>
+        <span className="newsletterText__span">
+          You can resign in any time.
+        </span>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Newsletter;
